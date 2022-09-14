515d33a828bf2e977fcef9c89902764892f918bf
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-14 19:01:10 +0530

    uc2 implement

--- EmpWageJava/src/com/EmpWage/uc_1.java
+++ EmpWageJava/src/com/EmpWage/uc_1.java
@@ -4,16 +4,26 @@ public class UC_1
 {
 	 public static void main(String[] args)
  {
-	int IS_PRESENT = 1;
-    double empcheck = Math.floor(Math.random()*10)%2;
-    
-    if (empcheck == IS_PRESENT)
-    {
-        System.out.println("Employee is Present");
-    }
-    else
-    {
-    	System.out.println("Employee is Absent");
-    }
- }
+		 int Emp_Rate_per_hour = 20;
+         int empHrs = 0;
+         int empWage = 0;
+         int Is_Full_Time = 1;
+         
+         double empcheck = Math.floor(Math.random()*10)%2;
+         if (empcheck == Is_Full_Time)
+         {
+             empHrs = 8;
+             System.out.println("Employee is Present");
+         }
+         else
+         {
+
+        	 System.out.println("Employee is Absent");
+        	 
+
+         }
+         empWage = empHrs * Emp_Rate_per_hour;
+         System.out.println("empwage is :" + empWage);
+
+}
 }

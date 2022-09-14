a96b7158688affc86c59dbe92cf86ff17d76ee50
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-14 16:36:18 +0530

    uc1 implement

---
+++ EmpWageJava/src/com/EmpWage/uc_1.java
@@ -0,0 +1,19 @@
+package com.EmpWage;
+
+public class uc_1 
+{
+	 public static void main(String[] args)
+ {
+	int IS_PRESENT = 1;
+    double empcheck = Math.floor(Math.random()*10)%2;
+    
+    if (empcheck == IS_PRESENT)
+    {
+        System.out.println("Employee is Present");
+    }
+    else
+    {
+    	System.out.println("Employee is Absent");
+    }
+ }
+}
